f0271137575ad80c812d8ecf67a69d6d6dfc6309
Victor Cortes <ie.cortex@gmail.com>
2013-11-13 18:56:27 -0500

    Move devise stuff to concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
-  include User::DeviseStuff
+  include DeviseStuff
 
   field :name, type: String
   field :email, type: String
